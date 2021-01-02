057681603d7ea395cbf1313bc0157f816254989c
mamba <nbapankui@163.com>
2021-01-02 23:49:43 +0800

    优化代码

--- algorithms-java-example/src/main/java/space.mamba/leetcode/algorithms/No122_Best_Time_to_Buy_and_Sell_Stock_2.java
+++ algorithms-java-example/src/main/java/space.mamba/leetcode/algorithms/No122_Best_Time_to_Buy_and_Sell_Stock_2.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.api.Assertions;
  * <p>
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
  *
+ * 典型的贪心算法的思想
  *
+ *
+ *  这道题由于可以无限次买入和卖出。我们都知道炒股想挣钱当然是低价买入高价抛出，那么这里我们只需要从第二天开始，
+ *  如果当前价格比之前价格高，则把差值加入利润中，因为我们可以昨天买入，今日卖出，若明日价更高的话，还可以今日买入，明日再抛出
  * <p>
  * 滑动窗口
  */
@@ -20,6 +24,8 @@ public class No122_Best_Time_to_Buy_and_Sell_Stock_2 {
         int value = obj.maxProfit(arr);
         System.out.println(value);
         Assertions.assertEquals(value, 7);
+
+        System.out.println(obj.maxProfit2(arr));
 
         System.out.println("###");
         int[] arr2 = {1,2,3,4,5};
@@ -62,4 +68,19 @@ public class No122_Best_Time_to_Buy_and_Sell_Stock_2 {
         return bestTimeSellStock;
     }
 
+    /**
+     * 局部最优解
+     *
+     * 多买就是最大的盈利
+     *
+     * */
+    public int maxProfit2(int[] prices) {
+        int res = 0;
+        for (int i = 0; i < prices.length - 1; ++i) {
+            if (prices[i] < prices[i + 1]) {
+                res += prices[i + 1] - prices[i];
+            }
+        }
+        return res;
+    }
 }
